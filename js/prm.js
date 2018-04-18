430d996d6599a85c5a4bb4b8c969ba30c878e143
dmittra <n01264481@humbermail.ca>
2018-04-17 23:29:49 -0400

    New

--- js/prm.js
+++ js/prm.js
@@ -21,31 +21,48 @@ function resetform() {
     document.getElementById('mail').value = '';
     document.getElementById('phn').value = '';
 }
-$(document).ready(function() {
-    $('#previous').on('click', function(){
-        $('#im_' + currentImage).stop().fadeOut(1);
-        decreaseImage();
-        $('#im_' + currentImage).stop().fadeIn(1);
-    });
-    $('#next').on('click', function(){
-        $('#im_' + currentImage).stop().fadeOut(1);
-        increaseImage();
-        $('#im_' + currentImage).stop().fadeIn(1);
-    });
-
-    var currentImage = 1;
-    var totalImages = 3;
-
-    function increaseImage() {
-        ++currentImage;
-        if(currentImage > totalImages) {
-            currentImage = 1;
-        }
+(function() {
+    function Slideshow(element) {
+        this.el = document.querySelector(element);
+        this.init();
     }
-    function decreaseImage() {
-        --currentImage;
-        if(currentImage < 1) {
-            currentImage = totalImages;
+    Slideshow.prototype = {
+        init: function() {
+            this.wrapper = this.el.querySelector(".slider-wrapper");
+            this.slides = this.el.querySelectorAll(".slide");
+            this.previous = this.el.querySelector(".slider-previous");
+            this.next = this.el.querySelector(".slider-next");
+            this.index = 0;
+            this.total = this.slides.length;
+            this.actions();
+        },
+        _slideTo: function(slide) {
+            var currentSlide = this.slides[slide];
+            this.wrapper.style.left = "-" + currentSlide.offsetLeft + "px";
+        },
+        actions: function() {
+            var self = this;
+            self.next.addEventListener("click", function() {
+                self.index++;
+                self.previous.style.display = "block";
+                if (self.index == self.total - 1) {
+                    self.index = self.total - 1;
+                    self.next.style.display = "none";
+                }
+                self._slideTo(self.index);
+            }, false);
+            self.previous.addEventListener("click", function() {
+                self.index--;
+                self.next.style.display = "block";
+                if (self.index == 0) {
+                    self.index = 0;
+                    self.previous.style.display = "none";
+                }
+                self._slideTo(self.index);
+            }, false);
         }
-    }
-});
+    };
+    document.addEventListener("DOMContentLoaded", function() {
+        var slider = new Slideshow("#main-slider");
+    });
+})();
