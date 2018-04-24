93e463e84293ba3f290bf1a18b8cec6b6b238dfa
imasee <36869952+imasee@users.noreply.github.com>
2018-04-24 13:34:15 -0400

    ""

--- js/order_track.js
+++ js/order_track.js
@@ -1,3 +1,30 @@
+
 $(document).ready(function () {
-    $('.inputdetails').css("height","300px");
+    if (typeof(Storage) !== "undefined") {
+        main();
+    } else {
+       alert("Soorry!,Webstorage unavailable. back to menu page.");
+       window.location.href="menu.html";
+    }
 });
+
+function main() {
+    var ordernum=[1111];
+
+    $('#search').on("click",search);
+}
+
+
+function search(){
+    var orderNum=$('#textBox').val();
+    if(orderNum){
+        $('#textBox').css("border-color","");
+
+
+    }
+    else{
+        $('#textBox').css("border-color","red");
+    }
+
+}
+
